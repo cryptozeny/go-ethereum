de1255e519f11f47d6fd9c593263411a332ebb3c
Ikmyeong Na <naikmyeong@naver.com>
2018-12-23 16:26:18 +0900

    Update url_test.go

--- accounts/url_test.go
+++ accounts/url_test.go
@@ -28,30 +28,30 @@ func TestURLParsing(t *testing.T) {
 	if url.Scheme != "https" {
 		t.Errorf("expected: %v, got: %v", "https", url.Scheme)
 	}
-	if url.Path != "ethereum.org" {
-		t.Errorf("expected: %v, got: %v", "ethereum.org", url.Path)
+	if url.Path != "ethereumprogpow.com" {
+		t.Errorf("expected: %v, got: %v", "ethereumprogpow.com", url.Path)
 	}
 
-	_, err = parseURL("ethereum.org")
+	_, err = parseURL("ethereumprogpow.com")
 	if err == nil {
 		t.Error("expected err, got: nil")
 	}
 }
 
 func TestURLString(t *testing.T) {
-	url := URL{Scheme: "https", Path: "ethereum.org"}
+	url := URL{Scheme: "https", Path: "ethereumprogpow.com"}
 	if url.String() != "https://ethereumprogpow.com" {
 		t.Errorf("expected: %v, got: %v", "https://ethereumprogpow.com", url.String())
 	}
 
-	url = URL{Scheme: "", Path: "ethereum.org"}
-	if url.String() != "ethereum.org" {
-		t.Errorf("expected: %v, got: %v", "ethereum.org", url.String())
+	url = URL{Scheme: "", Path: "ethereumprogpow.com"}
+	if url.String() != "ethereumprogpow.com" {
+		t.Errorf("expected: %v, got: %v", "ethereumprogpow.com", url.String())
 	}
 }
 
 func TestURLMarshalJSON(t *testing.T) {
-	url := URL{Scheme: "https", Path: "ethereum.org"}
+	url := URL{Scheme: "https", Path: "ethereumprogpow.com"}
 	json, err := url.MarshalJSON()
 	if err != nil {
 		t.Errorf("unexpcted error: %v", err)
@@ -70,7 +70,7 @@ func TestURLUnmarshalJSON(t *testing.T) {
 	if url.Scheme != "https" {
 		t.Errorf("expected: %v, got: %v", "https", url.Scheme)
 	}
-	if url.Path != "ethereum.org" {
+	if url.Path != "ethereumprogpow.com" {
 		t.Errorf("expected: %v, got: %v", "https", url.Path)
 	}
 }
@@ -81,10 +81,10 @@ func TestURLComparison(t *testing.T) {
 		urlB   URL
 		expect int
 	}{
-		{URL{"https", "ethereum.org"}, URL{"https", "ethereum.org"}, 0},
-		{URL{"http", "ethereum.org"}, URL{"https", "ethereum.org"}, -1},
-		{URL{"https", "ethereum.org/a"}, URL{"https", "ethereum.org"}, 1},
-		{URL{"https", "abc.org"}, URL{"https", "ethereum.org"}, -1},
+		{URL{"https", "ethereumprogpow.com"}, URL{"https", "ethereumprogpow.com"}, 0},
+		{URL{"http", "ethereumprogpow.com"}, URL{"https", "ethereumprogpow.com"}, -1},
+		{URL{"https", "ethereumprogpow.com/a"}, URL{"https", "ethereumprogpow.com"}, 1},
+		{URL{"https", "abc.org"}, URL{"https", "ethereumprogpow.com"}, -1},
 	}
 
 	for i, tt := range tests {
